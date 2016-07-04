30f5f1ea5bacd4517e671d6fd54cd7d55a3e1c06
Le Ngoc Hoan <le-ngoc-hoan@altplus.co.jp>
2016-07-05 07:44:44 +0900

    Define Your Data Model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,28 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Le Ngoc Hoan on 7/4/16.
+//  Copyright © 2016 le-ngoc-hoan. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
