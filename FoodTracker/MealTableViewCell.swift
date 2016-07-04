37016a289c18b233b1cd8657467dd763f29c542c
Le Ngoc Hoan <le-ngoc-hoan@altplus.co.jp>
2016-07-05 08:25:25 +0900

    Add Images to Your Project

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Le Ngoc Hoan on 7/5/16.
+//  Copyright © 2016 le-ngoc-hoan. All rights reserved.
+//
+
+import Cocoa
+
+class MealTableViewCell: UITableViewCell {
+
+}
